012de366ee723637bb1a16c950480bdfdc5b0bea
Michael <vuanhduc2685@gmail.com>
2020-07-19 23:03:48 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import {Navigation} from 'react-native-navigation';
+
+// Note:
+// in order to test Redux and MobX separately,
+// please comment unnecessary import, this is important
+// because RNN registers screens for both of them if two imports are presented
+
+import {startApp} from './src/App';
+
+Navigation.events().registerAppLaunchedListener(() => {
+  startApp();
+});
